fff9932ab145da4b29ef6db61e76c328e1050aa2
RandaB <fr_boukabene@esi.dz>
2021-08-08 20:33:18 +0100

    Find element by name and send value

--- demoDriver.py
+++ demoDriver.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 #through selenuim test we need to invoke the executable file which will then invoke actual
 driver = webdriver.Chrome(executable_path="C:\\Users\\s\\Documents\\driver\\chromedriver.exe")
 
-driver.get("https://selenium-python.readthedocs.io/installation.html")
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+driver.find_element_by_name("name").send_keys("Randa")
+
