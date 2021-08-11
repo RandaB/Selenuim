2319d0921bea9c0b8369bdd24d4ee0c91ed3dff0
RandaB <fr_boukabene@esi.dz>
2021-08-11 21:23:46 +0100

    check a checkbox

--- uiControls.py
+++ uiControls.py
@@ -8,3 +8,8 @@ driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 
 checkboxes = driver.find_elements_by_xpath("//input[@type='checkbox']")
 print(len(checkboxes))
+
+for checkbox in checkboxes:
+    if checkbox.get_attribute("value") == "option2":
+        checkbox.click()
+        assert checkbox.is_selected()
